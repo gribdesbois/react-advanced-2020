530a2a325cd81a18dcf22f7016c1f52200af0fa7
gribdesbois <gribdesbois@gmail.com>
2021-10-30 15:55:37 +0200

    complex increase setValue((prevState)=>{return prevState + 1}

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,14 +2,35 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0)
+  const reset = () => {
+    setValue(0)
+  }
+
+  const complexeIncrease = () => {
+    setTimeout(() => {
+      //setValue(value + 1)
+      setValue((prevState) => {
+        return prevState + 1
+      })
+    }, 2000)
+  }
+
+  
   return (
     <>
     <section style={{ margin: '4rem 0'}}>
       <h2>regular counter</h2>
       <h1>{value}</h1>
       <button className='btn' onClick={ () => setValue(value - 1)}> decrease</button> {/* ARROW FUNCTION */}
-      <button className='btn' onClick={ () => setValue(0)}> reset</button>
+      <button className='btn' onClick={reset}> reset</button>
       <button className='btn' onClick={ () => setValue(value + 1)}> increase</button>
+    </section>
+    <section style={{ margin: '4rem 0'}}>
+      <h2>more complex counter</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={complexeIncrease}>
+        increase later
+      </button>
     </section>
     </>
     )
