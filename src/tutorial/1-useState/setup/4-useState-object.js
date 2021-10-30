2a3e92198cd2cc95cfab2d300a3684a9c0c13648
gribdesbois <gribdesbois@gmail.com>
2021-10-30 15:24:44 +0200

    same with 3 hooks instead of an object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 
 const UseStateObject = () => {
   const [person, setPerson] = useState({name:'peter', age:24, message:'random message'})
+
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('random message')
+
   const changeMessage = () => {
-    setPerson({...person, message:'Hello world'}) /* //! SPREAD */
+    setMessage('hello world')
   }
   return (
     <>
-    <h3>{person.name}</h3>
-    <h3>{person.age}</h3>
-    <h3>{person.message}</h3>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
     <button className='btn' onClick={changeMessage}>
       change message
     </button>
