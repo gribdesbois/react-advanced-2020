6ad43e193f9b8108ae8ae432a409ce0aa091d9bf
gribdesbois <gribdesbois@gmail.com>
2021-10-30 15:04:38 +0200

    done

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -4,6 +4,11 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [ people, setPeople ] = React.useState(data)
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
   return (
     <> 
     {
@@ -12,11 +17,12 @@ const UseStateArray = () => {
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
           </div>
         )
       })
     }
-    <button className='btn' onClick={() => setPeople([])}>
+    <button className='btn' onClick={() => setPeople([])}> {/* arrow function to have setPeople invoked only when clicking */}
       clear items
     </button>
     </>
