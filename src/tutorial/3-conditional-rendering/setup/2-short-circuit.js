4ec7f0e428f330dbb0f08021811a36d53c5bea3a
gribdesbois <gribdesbois@gmail.com>
2021-10-31 14:40:06 +0100

    using ternary ? operator for conditional rendering

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -18,6 +18,13 @@ const ShortCircuit = () => {
     <h1>{text || 'john doe'}</h1>
     <button className='btn' onClick={() => setIsError(!isError)}> toggle error</button>
     {isError && <h1>Error...</h1>}
+    {isError ? (
+    <p>there is an error...</p>
+    ) : (
+    <div>
+      <h2>there is no error</h2>
+    </div>
+    )}
 
   </>
 }
